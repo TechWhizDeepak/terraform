0812e5e515931496d1282c688994a257e50a7501
deepakchugh4 <86863143+deepakchugh4@users.noreply.github.com>
2022-07-19 16:48:33 +0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,68 @@
+# Terraform Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-west-1"
+}
+
+resource "aws_instance" "deepak_new_1" {
+ instance_type = "t2.micro" 
+ ami     = data.aws_ami.amzlinux2.id
+ tags = {
+ Name = "deepak_instance"
+ }
+ user_data = file("${path.module}/data.sh")
+#  vpc_security_group_ids = [aws_security_group.allow_ports.id]
+   }
+
+
+
+#provisioner "remote-exec" {
+    #inline = [
+      #"sudo apt-get update && apt-get upgrade",
+      #"apt-get install vim",
+      #"apt-get install nginx-full"
+    #]
+
+
+
+#}
+
+# resource "aws_security_group" "allow_ports" {
+#    name        = "allow_ssh_http"
+#    description = "Allow inbound SSH traffic and http from any IP"
+
+#    #ssh access
+#    ingress {
+#        from_port   = 22
+#        to_port     = 22
+#        protocol    = "tcp"
+#        # Restrict ingress to necessary IPs/ports.
+#        cidr_blocks = ["0.0.0.0/0"]
+#    }
+
+#    # HTTP access
+#    ingress {
+#        from_port   = 80
+#        to_port     = 80
+#        protocol    = "tcp"
+#        # Restrict ingress to necessary IPs/ports.
+#        cidr_blocks = ["0.0.0.0/0"]
+#    }
+
+#    egress {
+#        from_port   = 0
+#        to_port     = 0
+#        protocol    = "-1"
+#        cidr_blocks = ["0.0.0.0/0"]
+#    }
+  
+# }
